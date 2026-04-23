52b3327e98b8152bf19a380c5d5143ded27b5fdb
iyong2887 <Indahndari84@gmail.com>
2026-04-23 17:33:51 +0700

    iyacantik

---
+++ shared/Train.java
@@ -0,0 +1,37 @@
+package shared;
+
+public class Train {
+    private String id;
+    private String origin;
+    private String destination;
+
+    // Constructor
+    public Trai(String id, String origin, String destination) {
+        this.id = id;
+        this.origin = origin;
+        this.desstination = destination;
+    }
+
+    // Getter
+    public String getID() {
+        return id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    // Optional: Setter (kalau mau fleksibel
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    // To
